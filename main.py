8e015b03accfd773b52b0c8bf8ee8cd108cbb53e
Rodrigo A. Cutiño A. <rodrigocutinoa@gmail.com>
2026-06-03 22:06:19 -0400

    feat: modelo de atos y visualización de productos

---
+++ main.py
@@ -0,0 +1,17 @@
+# main.py
+# Punto de entrada del programa
+ 
+from src.productos import crear_producto, imprimir_lista
+
+# --- Prueba del Día 2 ---
+productos = []
+
+p1 = crear_producto(1, "Leche", 1500.0, 10)
+p2 = crear_producto(2, "Pan", 800.0, 25)
+p3 = crear_producto(3, "Agua", 500.0, 50)
+
+productos.append(p1)
+productos.append(p2)
+productos.append(p3)
+
+imprimir_lista(productos)
---
+++ src/productos.py
@@ -0,0 +1,53 @@
+# src/productos.py
+# Responsabilidad: Lógica de gestión de productos
+
+
+# --------------------------------------------------
+# MODELO DE DATOS
+# --------------------------------------------------
+
+def crear_producto(id, nombre, precio, cantidad):
+        """
+    Crea y retorna un producto como diccionario.
+
+    Parámetros:
+        id       (int)   : Identificador único
+        nombre   (str)   : Nombre del producto
+        precio   (float) : Precio del producto
+        cantidad (int)   : Stock disponible
+
+    Retorna:
+        dict: Producto con sus atributos
+    """
+        return {
+                "id": id,
+                "nombre": nombre,
+                "precio": precio,
+                "cantidad": cantidad
+                }
+
+
+# --------------------------------------------------
+# MOSTRAR PRODUCTOS
+# --------------------------------------------------
+
+def imprimir_producto(producto):
+        """Imprime un producto formateado en pantalla"""
+        print(f"  ID       : {producto["id"]}")
+        print(f"  Nombnre  : {producto["nombre"]}")
+        print(f"  Precio   : {producto["precio"]}")
+        print(f"  Cantiad  : {producto["cantidad"]}")
+        print(f" {"-" * 30}")
+
+
+def imprimir_lista(productos):
+        if not productos:
+            print("No hay productos registrados")
+            return
+        
+        print(f"\n {"-" * 30}")
+        print(f"  LISTA DE PRODUCTOS")
+        print(f" {"-" * 30}\n")
+
+        for producto in productos:
+            imprimir_producto(producto)
